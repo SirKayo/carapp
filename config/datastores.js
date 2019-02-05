ba3f8fb9b2d9403cea119c74a7428ad9bcec86d1
SirKayo <kaloqn.siriyski@gmail.com>
2019-02-05 19:43:23 +0200

    Update datastores.js

--- config/datastores.js
+++ config/datastores.js
@@ -51,7 +51,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mongo',
-    url: 'mongodb://mongodb:27017/sail',
+    url: 'mongodb://127.0.0.1:27017/sail',
 
   },
 
